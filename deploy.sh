a19ba6fc2a90700762fb333d4f64c758524f1a3f
Gunnar Lund <gunnar.l.n@gmail.com>
2020-07-14 17:28:39 -0400

    Add stuff

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+stack build
+stack exec site rebuild
+
+git add .
+git commit -m "Update dev on $date."
+
+# Get previous files
+git fetch --all
+git push origin develop #push development branch
+
+git checkout master
+
+# Overwrite existing files with new files
+cp -a _site/. .
+
+# Commit
+git add -A
+git commit -m "Publish updates on $date."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout develop
+git stash pop
--- site.hs
+++ site.hs
@@ -90,17 +90,17 @@ postCtx =
 --need to think about how to deal with maybe values though
 --
 --This stuff all works fine, but I think I'm going to manually enter the stuff into html tables.
-splitPipe' :: [String] -> [(String, String)]
-splitPipe' s = map (break (=='|')) s
-
-insRow :: [(String, String)] -> [String]
-insRow [] = [""]
-insRow ((x, y:ys):ps) = ("\t<tr><td class=\"year\">"++x++"</td><td class=\"pub\">"++ys++"</td></tr>"):rest
-       where rest = insRow ps
-
-makeTable :: [String] -> String
-makeTable rs = "<table>\n"++(concat (map (++"\n") rs))++"</table>"
-
-makeTemp :: String -> String -> IO ()
-makeTemp _ "" = return ()
-makeTemp file x = writeFile file (makeTable . insRow . splitPipe' . lines $ x)
+-- splitPipe' :: [String] -> [(String, String)]
+-- splitPipe' s = map (break (=='|')) s
+--
+-- insRow :: [(String, String)] -> [String]
+-- insRow [] = [""]
+-- insRow ((x, y:ys):ps) = ("\t<tr><td class=\"year\">"++x++"</td><td class=\"pub\">"++ys++"</td></tr>"):rest
+--        where rest = insRow ps
+--
+-- makeTable :: [String] -> String
+-- makeTable rs = "<table>\n"++(concat (map (++"\n") rs))++"</table>"
+--
+-- makeTemp :: String -> String -> IO ()
+-- makeTemp _ "" = return ()
+-- makeTemp file x = writeFile file (makeTable . insRow . splitPipe' . lines $ x)
