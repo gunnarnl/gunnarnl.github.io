211096afa2a2ed8712145a7d8c62010163cfe8ce
Gunnar Lund <gunnar.l.n@gmail.com>
2020-07-14 17:25:17 -0400

    Publish updates on .

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+stack build
+stack exec site rebuild
+
+# Get previous files
+git fetch --all
+git push origin develop #push development branch
+
+git checkout master
+
+# Overwrite existing files with new files
+cp -a _site/. .
+
+# Commit
+git add -A
+git commit -m "Publish updates on $date."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout develop
+git stash pop
